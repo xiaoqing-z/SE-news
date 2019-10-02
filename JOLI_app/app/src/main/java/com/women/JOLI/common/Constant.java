f06aab9828d1e2bebc929eaace42a6f080482edf
MelkiorOno <45061193+MelkiorOno@users.noreply.github.com>
2019-10-02 01:12:07 -0500

    Update Constant.java

--- JOLI_app/app/src/main/java/com/women/JOLI/common/Constant.java
+++ JOLI_app/app/src/main/java/com/women/JOLI/common/Constant.java
@@ -1,17 +1,9 @@
 package com.women.JOLI.common;
 
-/**
- * ClassName: Constans<p>
- * Author: oubowu<p>
- * Fuction: 常量<p>
- * CreateDate: 2016/2/17 14:26<p>
- * UpdateUser: <p>
- * UpdateDate: <p>
- */
 public class Constant {
 
     public static final String DB_NAME = "NewsChannelTable";
 
-    public static final String APP_NAME = "OuNews";
+    public static final String APP_NAME = "JOLI";
 
 }
